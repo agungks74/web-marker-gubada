4b640d50235a0f8b9eeb5544d36686611e76856a
Agung Kurniawan Syahferdi <agungsyahferdi69@gmail.com>
2021-08-04 05:46:24 +0700

    Memperbaiki logika bisnis

---
+++ script/markerData.js
@@ -0,0 +1,84 @@
+const daftarBahasa = [
+  {
+    namaBahasa: 'Jawa',
+    daftarMateri: [
+      {
+        namaMateri: 'Pengenalan dan Aksara Jawa',
+        marker: [
+          'marker-carakan.jpg',
+          'marker-murda.jpg',
+          'marker-rekan.jpg',
+          'marker-swara.jpg',
+          'marker-sandhanganswara.jpg',
+          'marker-sandhanganpwanda.jpg',
+          'marker-penandakonsonan.jpg',
+        ],
+      },
+      {
+        namaMateri: 'Kata Ganti Orang (Tembung Tri Purusa)',
+        marker: ['marker-kataganti.jpg'],
+      },
+      {
+        namaMateri: 'Kata Tanya, Kata Tunjuk, Kata Sambung (Tembung Sabawa)',
+        marker: ['marker-katatanya.jpg'],
+      },
+      {
+        namaMateri: 'Bagian Tubuh (Péranganing Awak)',
+        marker: ['marker-bagiantubuh.jpg'],
+      },
+      {
+        namaMateri: 'Jenis Warna (Jinising Warna)',
+        marker: ['marker-jeniswarna.jpg'],
+      },
+      {
+        namaMateri: 'Ungkapan Keseharian (Tembung Padinan)',
+        marker: ['marker-ungkapan.jpg'],
+      },
+      {
+        namaMateri: 'Kata Kerja (Tembung Kriya)',
+        marker: ['marker-katakerja.jpg'],
+      },
+      {
+        namaMateri: 'Kata Sifat (Tembung Kahanan)',
+        marker: ['marker-katasifat.jpg'],
+      },
+    ],
+  },
+  {
+    namaBahasa: 'Batak Toba',
+    daftarMateri: [
+      {
+        namaMateri: 'Pelajaran Pertama (Penulisan dan Pelafalan)',
+        marker: [
+          'markerbatak-p1.1.jpg',
+          'markerbatak-p1.2.jpg',
+          'markerbatak-p1.3.jpg',
+          'marker-swara.jpg',
+          'marker-sandhanganswara.jpg',
+          'marker-sandhanganpwanda.jpg',
+        ],
+      },
+      {
+        namaMateri: 'Pelajaran Kedua (Pembendaharaan Kata)',
+        marker: ['marker-kataganti.jpg'],
+      },
+      {
+        namaMateri: 'Pelajaran Ketiga (Sapaan Atau Panggilan Kekerabatan)',
+        marker: ['marker-katatanya.jpg'],
+      },
+      {
+        namaMateri: 'Pelajaran Keempat (Pembendaharaan Kata)',
+        marker: ['marker-bagiantubuh.jpg'],
+      },
+      {
+        namaMateri:
+          'Pelajaran Kelima (Sapaan atau Panggilan Kekerabatan Terhadap Hula-hula. Terhadap Boru, dan Sesama Boru)',
+        marker: ['marker-jeniswarna.jpg'],
+      },
+      {
+        namaMateri: 'Pelajaran Keenam (Belajar Praktis Aksara Batak)',
+        marker: ['marker-ungkapan.jpg'],
+      },
+    ],
+  },
+];
--- script/script.js
+++ script/script.js
@@ -1,104 +1,67 @@
-const markerAksara = ['marker-carakan.jpg','marker-murda.jpg','marker-rekan.jpg','marker-swara.jpg','marker-sandhanganswara.jpg','marker-sandhanganpwanda.jpg','marker-penandakonsonan.jpg']
-const aButton = document.querySelectorAll('.list-group-item');
 const images = document.querySelector('.carousel-inner');
 const marker = document.createElement('img');
-marker.setAttribute('class', 'd-block mx-auto')
+const listMateri = document.querySelector('.list-group');
+const markerImages = document.querySelector('.carousel-inner');
 
-for(let aItem of aButton){
+let bahasa;
+
+function changeLang() {
+  const v = document.getElementById('langSelect');
+
+  switch (v.value) {
+    case daftarBahasa[0].namaBahasa:
+      bahasa = daftarBahasa[0];
+      break;
+    case daftarBahasa[1].namaBahasa:
+      bahasa = daftarBahasa[1];
+      break;
+  }
+  changeMain();
+}
+
+function changeMain() {
+  listMateri.innerHTML = '';
+
+  const mat = bahasa.daftarMateri;
+
+  for (let i = 0; i < mat.length; i++) {
+    const aItem = document.createElement('a');
+    aItem.setAttribute('href', '#');
+    aItem.setAttribute('class', 'list-group-item list-group-item-action');
+    aItem.innerText = mat[i].namaMateri;
+
+    if (i === 0) {
+      aItem.classList.add('active');
+    }
+
     aItem.addEventListener('click', (event) => {
-    const activeItem = document.querySelector('a.active')
-    activeItem.classList.remove('active');
-    event.target.classList.add('active');
-    
-    images.innerHTML = '';
-    
-    switch(+event.target.id){
-        case 1:
-            const cItem0 = document.createElement('div');
-            cItem0.setAttribute('class', 'carousel-item active')
+      markerImages.innerHTML = '';
 
-            const marker0 = document.createElement('img');
-            marker0.setAttribute('src', 'img/marker/' + markerAksara[0]);
-            marker0.setAttribute('class', 'd-block mx-auto')
-            cItem0.appendChild(marker0);
+      const activeItem = document.querySelector('a.active');
+      activeItem.classList.remove('active');
+      event.target.classList.add('active');
 
-            const cItem1 = document.createElement('div');
-            cItem1.setAttribute('class', 'carousel-item')
+      const markers = mat[i].marker;
 
-            const marker1 = document.createElement('img');
-            marker1.setAttribute('src', 'img/marker/' + markerAksara[1]);
-            marker1.setAttribute('class', 'd-block mx-auto')
-            cItem1.appendChild(marker1);
+      console.log(markers);
 
+      for (let img = 0; img < markers.length; img++) {
+        const aItem = document.createElement('img');
+        aItem.setAttribute('src', 'img/marker/' + markers[img]);
+        aItem.setAttribute('class', 'd-block mx-auto');
 
-            const cItem2 = document.createElement('div');
-            cItem2.setAttribute('class', 'carousel-item')
+        const aCorouselItem = document.createElement('div');
+        aCorouselItem.setAttribute('class', 'carousel-item');
 
-            const marker2 = document.createElement('img');
-            marker2.setAttribute('src', 'img/marker/' + markerAksara[2]);
-            marker2.setAttribute('class', 'd-block mx-auto')
-            cItem2.appendChild(marker2);
+        if (img === 0) {
+          aCorouselItem.classList.add('active');
+        }
 
-            const cItem3 = document.createElement('div');
-            cItem3.setAttribute('class', 'carousel-item')
+        aCorouselItem.append(aItem);
+        markerImages.append(aCorouselItem);
+      }
+    });
 
-            const marker3 = document.createElement('img');
-            marker3.setAttribute('src', 'img/marker/' + markerAksara[3]);
-            marker3.setAttribute('class', 'd-block mx-auto')
-            cItem3.appendChild(marker3);
-
-            const cItem4 = document.createElement('div');
-            cItem4.setAttribute('class', 'carousel-item')
-
-            const marker4 = document.createElement('img');
-            marker4.setAttribute('src', 'img/marker/' + markerAksara[4]);
-            marker4.setAttribute('class', 'd-block mx-auto')
-            cItem4.appendChild(marker4);
-
-            const cItem5 = document.createElement('div');
-            cItem5.setAttribute('class', 'carousel-item')
-
-            const marker5 = document.createElement('img');
-            marker5.setAttribute('src', 'img/marker/' + markerAksara[5]);
-            marker5.setAttribute('class', 'd-block mx-auto')
-            cItem5.appendChild(marker5);
-
-            const cItem6 = document.createElement('div');
-            cItem6.setAttribute('class', 'carousel-item')
-
-            const marker6 = document.createElement('img');
-            marker6.setAttribute('src', 'img/marker/' + markerAksara[6]);
-            marker6.setAttribute('class', 'd-block mx-auto')
-            cItem6.appendChild(marker6);
-
-            console.log(cItem0)
-            images.append(cItem0,cItem1,cItem2,cItem3,cItem4,cItem5,cItem6);
-            return;
-        break;
-        case 2:
-            marker.setAttribute('src', 'img/marker/marker-kataganti.jpg');
-        break;
-        case 3:
-        marker.setAttribute('src', 'img/marker/marker-katatanya.jpg');
-         
-        break;
-        case 4:
-        marker.setAttribute('src', 'img/marker/marker-bagiantubuh.jpg');
-        break;
-        case 5:
-        marker.setAttribute('src', 'img/marker/marker-jeniswarna.jpg');
-        break;
-        case 6:
-        marker.setAttribute('src', 'img/marker/marker-ungkapan.jpg');
-        break;
-        case 7:
-        marker.setAttribute('src', 'img/marker/marker-katakerja.jpg');
-        break;
-        case 8:
-        marker.setAttribute('src', 'img/marker/marker-katasifat.jpg');
-        break;
-         }
-         images.appendChild(marker);
-
-    })
+    listMateri.append(aItem);
+  }
 }
